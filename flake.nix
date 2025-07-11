f98dcf63945a77572b67e34bbdade48706b1c766
rssAgus <arusso@fixear.net>
2025-07-11 15:25:53 -0300

    changes

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         modules = [
           ./system/configuration.nix
           ./system/hardware-configuration.nix
-          ./pkgs/packs.nix
+          ./system/pkgs/packs.nix
         ];
       };
     };
