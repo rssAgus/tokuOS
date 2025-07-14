ca24685c1208ca0d0af3b9deb48a89bb3b9306f9
rssAgus <arusso@fixear.net>
2025-07-14 13:40:36 -0300

    rootless?

--- system/bash.nix
+++ system/bash.nix
@@ -1,8 +1,10 @@
 { config, pkgs, ... }: 
 {
 
+  security.sudo.wheelNeedsPassword = false;
+
   programs.bash.shellAliases = {
-    rebuild = "sudo nixos-rebuild switch";
+    rebuild = "cd repos/github/mynix-doties/ && sudo nixos-rebuild switch --flake .#rssagus";
     configure-dots = "cd repos/github/mynix-doties/ && code .";
   };
 
