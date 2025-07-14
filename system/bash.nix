25dda423c740cebbb9b407dc9784e28ededdc408
rssAgus <arusso@fixear.net>
2025-07-14 13:53:09 -0300

    FORGOT TO REBUILD

--- system/bash.nix
+++ system/bash.nix
@@ -5,7 +5,7 @@
   
   # Aliases
   programs.bash.shellAliases = {
-    rebuild = "nixos-rebuild switch --flake /home/agustin/repos/github/mynix-doties";
+    rebuild = "sudo nixos-rebuild switch --flake /home/agustin/repos/github/mynix-doties";
     configure-dots = "cd repos/github/mynix-doties/ && code .";
   };
 
